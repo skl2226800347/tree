d0fce445a0971b3f6f3e528a6a7245e84de4319a
石可磊 <shikelei@yuantutech.com>
2022-10-10 17:15:26 +0800

    b plus tree

--- b-plus-tree/src/main/java/com/skl/tree/Application.java
+++ b-plus-tree/src/main/java/com/skl/tree/Application.java
@@ -5,8 +5,18 @@ import com.skl.tree.file.TreeMappedFile;
 public class Application {
     private static final int DEGREE = 4;
     private static final String FILE_NAME ="d:/2.log";
+    public static TreeMappedFile treeMappedFile = new TreeMappedFile(FILE_NAME);
+    static {
+        Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
+            @Override
+            public void run() {
+                treeMappedFile.cleanup();
+            }
+        }));
+    }
+
     public static final void main(String[]args) throws Exception{
-        TreeMappedFile treeMappedFile = new TreeMappedFile(FILE_NAME);
+
         BPlusTree bPlusTree = new BPlusTree(DEGREE,treeMappedFile);
         bPlusTree.insert("333");
     }
